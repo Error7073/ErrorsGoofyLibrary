fd4e274c06e0c4aad32e21e58dc5f6212c3979ce
ScriptaMan#2635 <108091602+Error7073@users.noreply.github.com>
2022-12-03 17:14:02 +0200

    Update GAL.lua

--- GAL.lua
+++ GAL.lua
@@ -1,10 +1,10 @@
 local GAL = {
+    ["WebhookUrl"] = "",
     ["Req"] = http_request or request or HttpPost or syn.request
 }
 
 function GAL:Execute(Arg)
 	if Arg["Do"] == "CheckWhitelist" then
-		local WebhookUrl = ""
 		local http_request = http_request;
 		local http_request = http_request;
 		local WebhookCheck = if getexecutorname and type(getexecutorname) == "function" then getexecutorname() else is_sirhurt_closure and "Sirhurt" or pebc_execute and "ProtoSmasher" or syn and "Synapse X" or secure_load and "Sentinel" or KRNL_LOADED and "Krnl" or SONA_LOADED and "Sona" or "Kid with shit exploit"
@@ -32,7 +32,7 @@ function GAL:Execute(Arg)
 			hwid = Headers[S]
     end
 end
-GAL["Req"]({Url = WebhookUrl, Body = game:GetService("HttpService"):JSONEncode({["content"] = "Looks like "..game.Players.LocalPlayer.Name.." ".."just executed your script.",["username"] = "Quandale Logger",["avatar_url"] = "https://i0.wp.com/www.followchain.org/wp-content/uploads/2022/07/quandale-dingle-real-person-2.jpg?resize=512%2C512&ssl=1",["embeds"] = {{["title"] = "__**Script Executed**__",["description"] = "**Info:**",["type"] = "rich",["color"] = tonumber(0xffffff),["fields"] = {{["name"] = "__Username:__",["value"] = game.Players.LocalPlayer.Name,["inline"] = true},{["name"] = "__ID:__",["value"] = game.Players.LocalPlayer.UserId,["inline"] = true},{["name"] = "__HWID:__",["value"] = hwid,["inline"] = true},{["name"] = "__Executor:__",["value"] = WebhookCheck,["inline"] = true},{["name"] = "IP:flushed:",["value"] = "Ayo",["inline"] = true}}}}}), Method = "POST", Headers = {["content-type"] = "application/json"}})
+GAL["Req"]({Url = GAL["WebhookUrl"], Body = game:GetService("HttpService"):JSONEncode({["content"] = "Looks like "..game.Players.LocalPlayer.Name.." ".."just executed your script.",["username"] = "Quandale Logger",["avatar_url"] = "https://i0.wp.com/www.followchain.org/wp-content/uploads/2022/07/quandale-dingle-real-person-2.jpg?resize=512%2C512&ssl=1",["embeds"] = {{["title"] = "__**Script Executed**__",["description"] = "**Info:**",["type"] = "rich",["color"] = tonumber(0xffffff),["fields"] = {{["name"] = "__Username:__",["value"] = game.Players.LocalPlayer.Name,["inline"] = true},{["name"] = "__ID:__",["value"] = game.Players.LocalPlayer.UserId,["inline"] = true},{["name"] = "__HWID:__",["value"] = hwid,["inline"] = true},{["name"] = "__Executor:__",["value"] = WebhookCheck,["inline"] = true},{["name"] = "IP:flushed:",["value"] = "Ayo",["inline"] = true}}}}}), Method = "POST", Headers = {["content-type"] = "application/json"}})
 		local function R()
             	for I, S in pairs(Arg["L"] or {}) do
                 		if S == hwid then
