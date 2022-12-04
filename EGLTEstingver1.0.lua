43992ba30b3bdf7ce92ae7e806f4b8766d4c15ce
ScriptaMan#2635 <108091602+Error7073@users.noreply.github.com>
2022-12-04 20:31:24 +0200

    Update and rename EGLTEstingv1.0.lua to EGLTEstingver1.0.lua

--- EGLTEstingver1.0.lua
+++ EGLTEstingver1.0.lua
@@ -92,7 +92,7 @@ EGL["Req"]({Url = EGL["Webhook"]["WebhookUrl"], Body = game:GetService("HttpServ
 end
 elseif Arg["Use"] == "Godmode" then
         game.ReplicatedStorage.BurnDamage:FireServer(game.Players.LocalPlayer.Character.Humanoid, CFrame.new(), 0 * math.huge, 0, Vector3.new(), "rbxassetid://241837157", 0, Color3.new(), "", 0, 0)
-		if Arg["AntiFling"] and type(Arg["AntiFling"]) == "true" then
+		if Arg["AntiFling"] == "true" then
 		game.RunService.Stepped:Connect(function()
                 if EGL["GodMode"]["AntiAnchor"] then
 				for Index, Value in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
@@ -137,7 +137,7 @@ elseif Arg["Use"] == "Godmode" then
                 end)
             end
 		end
-	if Arg["GodmodeLoop"] and type(Arg["GodmodeLoop"]) == "true" then
+	if Arg["GodmodeLoop"] == "true" then
         game.Players.LocalPlayer.CharacterAdded:Connect(function(chr)
             wait(2)
             game.ReplicatedStorage.BurnDamage:FireServer(chr.Humanoid, CFrame.new(), 0 * math.huge, 0, Vector3.new(), "rbxassetid://241837157", 0, Color3.new(), "", 0, 0)
