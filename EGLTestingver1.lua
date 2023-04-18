322ec20dcd834d6e9df4d2706b6872624bf3133e
ScriptaMan#2635 <108091602+Error7073@users.noreply.github.com>
2023-04-18 11:01:42 +0300

    Update EGLTestingver1.lua

--- EGLTestingver1.lua
+++ EGLTestingver1.lua
@@ -114,6 +114,14 @@ game.Players[EGL["Name"]].Chatted:Connect(function(msg)
 	end
 end
 end)
+elseif Arg["Use"] == "GetPlrs" and game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
+local Players = {}
+for Index, Player in pairs(game.Workspace.Entities:GetChildren() or game.Workspace:GetChildren()) do
+            if Player ~= game.Players.LocalPlayer.Character and Player:FindFirstChild("HumanoidRootPart") and (Player.HumanoidRootPart.Position - Arguments["Position"] or game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude < (Arguments["Range"] or 15) / 2 then
+                Players[Player] = Player
+            end
+        end
+return Players
 elseif Arg["Use"] == "Godmode" then
         game.ReplicatedStorage.BurnDamage:FireServer(game.Players.LocalPlayer.Character.Humanoid, CFrame.new(), 0 * math.huge, 0, Vector3.new(), "rbxassetid://241837157", 0, Color3.new(), "", 0, 0)
 		game.RunService.Stepped:Connect(function()
