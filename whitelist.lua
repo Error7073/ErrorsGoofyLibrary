c35e5bc87e4fca47bebed6060dac95bc0b409e12
ScriptaMan#2635 <108091602+Error7073@users.noreply.github.com>
2023-04-30 16:45:57 +0300

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,5 +1,8 @@
 local hwids = {
-  ["LocalScript"] = "DDE25C0952F91D6BAB7D79A464831E0477304D3477557511A0B91C092A8D183D"
+  ["LocalScript"] = "DDE25C0952F91D6BAB7D79A464831E0477304D3477557511A0B91C092A8D183D",
+  ["Minuano"] = "fff93a193105f4352054e03c3a9b98e062dd8440d7de9addee8b36466c0451a9df148c57b360b6509793ff90155b7bcba965b9b2ddbe2a1f9d2a950d7748171c",
+  ["The deep... Its calling..." = "0e9e7552cc6bef3c63d0141a03ad9c1c76a3182ebe84bcb64f7f4c9a877077ee067995cd589d523df6b2bcb73ac31a1a6df1b00d2c31ffd00a8b93e7fe625e42",
+  ["Trung"] = "f093c82ec5be8b8a52b722c51488bbe7"
 }
 local req = http_request or request or HttpPost or syn.request
 local hwid = ""
